693e7cdd86f4ccfac62b1e0975e1048319915536
jenninea <alaa.jennine@grenoble-inp.org>
2025-08-12 18:02:42 +0200

    hh

--- src/Studies.js
+++ src/Studies.js
@@ -3,15 +3,20 @@ import './Studies.css';
 function Studies() {
   return (
     <div className="Studies-page">
-      <div className="study-item">
+      <div className="study-item1">
         <h2>Ensimag - MOSIG Program</h2>
         <p>Currently enrolled in the MOSIG program at Ensimag, focusing on software engineering and computer science.</p>
       </div>
-      <div className="study-item">
-        <h2>Previous Education</h2>
-        <p>Bachelor's degree in Computer Science from XYZ University, where I developed a strong foundation in programming and algorithms.</p>
+      <div className="study-item2">
+        <h2>Bachelor's degree</h2>
+        <p>Bachelor's degree in Computer Science from Grenoble-INP, where I developed a strong foundation in programming and algorithms.</p>
+      </div>
+      <div className='study-item3'>
+        <h2>MPSI-MP*</h2>
+        <p>Two intensive years of mathematics , computer science physics to prepare to the concours for engineering schools</p>
       </div>
     </div>
+
   );
 }
 
