0970f72bf896584de5bb1bd5f6283b10da7f4d9d
jenninea <alaa.jennine@grenoble-inp.org>
2025-09-04 22:42:41 +0200

    hh

--- src/Projects.js
+++ src/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
         <div className="Projects-page">
             <div className="project-item1">
                 <h2>Web Application</h2>
-                <p>Developed a website for a cleaning airbnb service interactive representing the service with a possibility to contact the provider</p>
+                <p>Created an interactive platform for an Airbnb cleaning service, highlighting the business offering and enabling direct customer contact.</p>
                 {/* <a href="https://abconciergerie.com" target="_blank" rel="noopener noreferrer"> */}
                     <img src={image} alt="Description de l'image" />
                 {/* </a> */}
@@ -16,7 +16,7 @@ function Projects() {
             
             <div className="project-item2">
                 <h2>Encoder ppm to jpeg</h2>
-                <p>Implemented a C program that converts images from PPM format to JPEG format </p>
+                <p>Developed a C-based image encoder capable of transforming raw PPM files into compressed JPEG format. </p>
                 {/* <img src={structure} alt="Description de l'image" /> */}
                 <a href="https://github.com/alaajee/encodeur-ppmtojpeg" target="_blank" rel="noopener noreferrer" className="ppm">GitHub Repository</a>
             </div>
@@ -24,7 +24,7 @@ function Projects() {
 
             <div className="project-item3">
                 <h2>Compiler</h2>
-                <p>Implemented a Java compiler </p>
+                <p>Developed a full Java compiler for the Deca programming language (Java-inspired), covering lexical and syntactic analysis, semantic verification, and code generation for both object-oriented and non-object-oriented constructs.</p>
                 {/* <img src={structure} alt="Description de l'image" /> */}
                 <a href="https://github.com/alaajee/Compilateur-Deca" target="_blank" rel="noopener noreferrer" className="ppm">GitHub Repository</a>
             </div>
@@ -32,14 +32,14 @@ function Projects() {
 
             <div className="project-item4">
                 <h2>Simulateur pompier</h2>
-                <p>Implemented a simulateur pompier using java and djikstra algorithm  </p>
+                <p>Developed a Java-based firefighting simulator that applies Dijkstra’s shortest path algorithm to optimize route planning and resource allocation. </p>
                 <video src={videoo} controls/>
                 <a href="https://github.com/alaajee/Projet_java" target="_blank" rel="noopener noreferrer" className="ppm">GitHub Repository</a>
             </div>
 
             <div className="project-item5">
                 <h2>Linux module</h2>
-                <p>Implemented alinux module in c for dag graph </p>
+                <p>Developed a Linux kernel module in C that handles multiple clients simultaneously, processes graph operations (PUT/GET) via an in-kernel bucket-based data store, executes CPU-intensive tasks, and returns responses efficiently. </p>
                 <a href="https://github.com/alaajee/Kernel_work" target="_blank" rel="noopener noreferrer" className="ppm">GitHub Repository</a>
             </div>
 
